75d442b5b4436da78a55bdf2e9aa248c395593a3
Julio Rafael Botta <jrb12494@gmail.com>
2024-03-13 03:20:19 -0300

    se completó la configuración inical d backend

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const cors=require('cors');
 const morgan=require('morgan');
+const path=require('path');
 
 
 //primera configuracion: crear una instancia de express
@@ -16,3 +17,36 @@ app.set('port', process.env.PORT || 9001);
 //ponemos a escuchar en un puerto a nuestro backend
 app.listen(app.get('port'), ()=>{console.log(`backend productos listening in port ${app.get('port')}`);});
 console.log("probando la actualizancion de nodemon");
+
+//middlewares: configuraciones extras del backend que se ejecutan antes de las rutas
+//1-middlewares nativos de express
+app.use(express.json());// permite recibir objetos en formato json.
+app.use(express.urlencoded({extended:true})); //permite recibir objeto de todo en las peticiones.
+//2-middlewares de terceros
+app.use(morgan('dev')); // nos proporciona detalles de las peticiones en la terminal.
+app.use(cors());//nos permite las peticiones remotas
+
+//cargar archivos estaticos que va a ser el index.html
+//console.log(__dirname, "DIRNAME");
+app.use(express.static(path.join(__dirname,'../public')));
+
+
+//creamos una ruta de prueba
+/*TIPO DE PETICIONES
+1)GET: obtener, pedir, leer.
+2)PUT/PATCH: actualizar
+3)POST: crear y enviar información desde el cliente al backend
+4)DELETE: borrar, eliminar.
+*/
+//req=request(contienen toda la info de la peticion del cliente al servidor)
+//res=response(contiene toda la info de la respuesta del servidor al cliente)
+//next indica que continue con la siguiente funcion o middleware
+app.get('/test', async(req, res, next)=>{
+    try {
+        console.log("REQUEST-->", req);
+        return res.status(200).json({success:true, message:"API IS ALIVE"});
+    } catch (error) {
+        console.error(error);
+        next(error);
+    }
+});
